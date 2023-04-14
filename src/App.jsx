e73e4742bda359c496b0d5d9bbe2eb06a230fdcd
Brandon Palmeros <brandonpaldom@gmail.com>
2023-04-13 23:12:39 -0600

    React Router installed and configured

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import lightTheme from './themes/lightTheme'
 import darkTheme from './themes/darkTheme'
+import Root from './routes/Root'
 
 export default function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       <CssBaseline />
+      <Root />
     </ThemeProvider>
   )
 }
